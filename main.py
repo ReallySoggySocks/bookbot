a26ae5d6e5d1430183c2c9aeb570546a465f13c1
Butter <havenkain@gmail.com>
2024-10-26 17:42:46 -0500

    main.py prints report on char count and word count

--- main.py
+++ main.py
@@ -3,6 +3,35 @@ def main():
 
         file_contents = f.read()
 
-        print(file_contents)
+        words = file_contents.split()
+        wc = len(words)
+
+        lowered = file_contents.lower()
+
+        char = {}
+        cc = [
+            {"letter": 'a', "num": 7}
+        ]
+
+        for chars in lowered:
+            if chars.isalpha():
+                if chars in char:
+                    char[chars] += 1
+                else:
+                    char[chars] = 1
+            else:
+                pass
+
+        print(
+            "--- Begin report of books/frankenstein.txt --- \n", 
+            f"{wc} words found in the document \n\n",
+            )
+        
+        for letter in char:
+            print(f"The '{letter}' character was found {char[letter]} number of times")
+
+        print("--- End Report --- \n")
+
+        return
 
 main()
